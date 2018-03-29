1f25127e34eb4bb35523c06e44827f1db9fe3530
HarisIrwansyah <32352423+HarisIrwansyah@users.noreply.github.com>
2018-03-29 10:15:02 +0800

    Membuat Atribut dan Behavior

--- src/praktek002/Rectangle.java
+++ src/praktek002/Rectangle.java
@@ -1,4 +1,12 @@
 package praktek002;
 public class Rectangle {
+    double panjang;
+    double lebar;
     
+    void CetakInfo(){
+        System.out.println("###################");
+        System.out.println("Panjang Persegi Panjang : "+panjang);
+        System.out.println("Lebar Persegi Panjang   : "+lebar);
+        System.out.println("");
+    }
 }
