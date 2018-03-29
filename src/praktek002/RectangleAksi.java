7314a39c41b4019e0d8449f2e30e6f602ee011b4
HarisIrwansyah <32352423+HarisIrwansyah@users.noreply.github.com>
2018-03-29 10:26:24 +0800

    Mengisi nilai Atribut

--- src/praktek002/RectangleAksi.java
+++ src/praktek002/RectangleAksi.java
@@ -4,6 +4,9 @@ public class RectangleAksi {
     public static void main(String[] args) {
         
         Rectangle r1 = new Rectangle();
+            
+            r1.panjang = 10;
+            r1.lebar = 5;
     }
     
 }
